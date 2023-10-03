ac48e231952009e1fdb31844233b9a3d543d010b
Fogo Adepitan <Fogoadepitan100@gmail.com>
2023-10-03 05:26:03 +0100

    update

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../components/HomeScreen.css";
+import Nav from "../components/Nav/Nav.js";
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  );
+}
+
+export default HomeScreen;
