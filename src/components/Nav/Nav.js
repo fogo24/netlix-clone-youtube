747281e7edd408b3fa25ba233c73957c2f2fbf1d
Fogo Adepitan <Fogoadepitan100@gmail.com>
2023-10-03 05:11:28 +0100

    features&components folder

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Nav/Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
+        alt=""
+      />
+      <h1>This is a nav </h1>
+    </div>
+  );
+}
+
+export default Nav;
