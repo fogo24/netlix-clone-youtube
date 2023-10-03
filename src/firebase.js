948ca603e41db2a9f4902df6167cfb1e0230e292
Fogo Adepitan <Fogoadepitan100@gmail.com>
2023-10-03 05:03:35 +0100

    created firebase json

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXARxnRiD_w9dMKn7thO48XHSSE53dzJk",
+  authDomain: "netflix-clone-c0b03.firebaseapp.com",
+  projectId: "netflix-clone-c0b03",
+  storageBucket: "netflix-clone-c0b03.appspot.com",
+  messagingSenderId: "1077639797525",
+  appId: "1:1077639797525:web:25904c396bcc325ab5a71c",
+};
+
+// const app = initializeApp(firebaseConfig);
