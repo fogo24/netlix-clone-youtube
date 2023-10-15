e68275e02e5fffca1b1c87a30433f8532722a8f8
Fogo Adepitan <Fogoadepitan100@gmail.com>
2023-10-15 09:14:29 +0100

    styling navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,11 +4,18 @@ import "../Nav/Nav.css";
 function Nav() {
   return (
     <div className="nav">
-      <img
-        src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
-        alt=""
-      />
-      <h1>This is a nav </h1>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
